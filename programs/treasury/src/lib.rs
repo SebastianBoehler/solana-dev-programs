bdddf0cca8d49ee4db9357fac2ab96de960bd73e
Sebastian Boehler <basti.boehler@hotmail.de>
2024-01-10 18:59:01 +0700

    feat: pullBack funds working

--- programs/treasury/src/lib.rs
+++ programs/treasury/src/lib.rs
@@ -1,14 +1,14 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Token, TokenAccount, Transfer as SplTransfer};
+use anchor_spl::token::{Token, TokenAccount, Transfer};
 use anchor_spl::token::Mint;
 
-declare_id!("5fFfVZDnmvBWsAsJcwdkxbAQ7yqRaX5H4Kk9wZn3DGiC");
+declare_id!("5hFG4V19ktLKzKjUAxcBs1dNBv5UFQPDsfVfgNvLWBAe");
 
 #[program]
 pub mod treasury {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, identifier: u64, amount: u64, _bump: u8) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, identifier: u64, amount: u64, pda_bump: u8) -> Result<()> {
         let escrow = &mut ctx.accounts.escrow_pda;
         escrow.identifier = identifier;
         escrow.user_sending = ctx.accounts.user_sending.key().clone();
@@ -16,7 +16,7 @@ pub mod treasury {
         escrow.token_mint = ctx.accounts.mint_of_token_being_sent.key().clone();
         escrow.amount = amount;
 
-        let transfer_instruction = SplTransfer{
+        let transfer_instruction = Transfer{
             from: ctx.accounts.users_sending_token_account.to_account_info(),
             to: ctx.accounts.escrow_wallet.to_account_info(),
             authority: ctx.accounts.user_sending.to_account_info(),
@@ -24,14 +24,15 @@ pub mod treasury {
 
         //seeds generation
         //let mint_of_token_being_sent_pk = ctx.accounts.mint_of_token_being_sent.key().clone();
-        let identifier_bytes = identifier.to_le_bytes();
+        //let identifier_bytes = identifier.to_le_bytes();
+        let bump_bytes = pda_bump.to_le_bytes();
         let inner = vec![
             b"state".as_ref(),
             ctx.accounts.user_sending.key.as_ref(),
             ctx.accounts.user_receiving.key.as_ref(),
             //mint_of_token_being_sent_pk.as_ref(), 
-            identifier_bytes.as_ref(),
-            //add bump
+            //identifier_bytes.as_ref(),
+            bump_bytes.as_ref(),
         ];
         let outer = vec![inner.as_slice()];
 
@@ -49,10 +50,48 @@ pub mod treasury {
 
         Ok(())
     }
+
+    pub fn pull_back(ctx: Context<PullBackInstruction>, _identifier: u64, pda_bump: u8) -> Result<()> {
+        let escrow = &ctx.accounts.escrow_pda;
+        let amount = escrow.amount;
+
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.escrow_wallet.to_account_info(),
+            to: ctx.accounts.users_sending_token_account.to_account_info(),
+            authority: ctx.accounts.escrow_pda.to_account_info(),
+        };
+
+        //let identifier_bytes = identifier.to_le_bytes();
+        let bump_bytes = pda_bump.to_le_bytes();
+        let inner = vec![
+            b"state".as_ref(),
+            ctx.accounts.user_sending.key.as_ref(),
+            ctx.accounts.user_receiving.key.as_ref(),
+            //mint_of_token_being_sent_pk.as_ref(), 
+            //identifier_bytes.as_ref(),
+            bump_bytes.as_ref(),
+        ];
+        let outer = vec![inner.as_slice()];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            outer.as_slice(),
+        );
+
+        anchor_spl::token::transfer(cpi_ctx, amount)?;
+
+        msg!("Funds transferred out! {}", amount);
+
+        //set escrow amount to 0
+        *&mut ctx.accounts.escrow_pda.amount = 0;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-#[instruction(identifier: u64, bump: u8)]
+#[instruction(identifier: u64, pda_bump: u8)]
 pub struct Initialize<'info> {
     #[account(mut)]
     user_sending: Signer<'info>,   
@@ -64,9 +103,9 @@ pub struct Initialize<'info> {
     #[account(
         init,
         //space = 8 + u64 + pubkey + pubkey + pubkey + u64 + u8 + enum
-        space = 8 + 32 + 32 + 32 + 8 + 1 + 1,
+        space = 8 + 8 + 32 + 32 + 32 + 8 + 1,
         payer = user_sending,
-        seeds=[b"state".as_ref(), user_sending.key().as_ref(), user_receiving.key().as_ref(), identifier.to_le_bytes().as_ref()],
+        seeds=[b"state".as_ref(), user_sending.key().as_ref(), user_receiving.key().as_ref()],
         bump,
     )]
     escrow_pda: Account<'info, State>,
@@ -74,10 +113,10 @@ pub struct Initialize<'info> {
     #[account(
         init,
         payer = user_sending,
-        seeds=[b"wallet".as_ref(), user_sending.key().as_ref(), user_receiving.key().as_ref(), identifier.to_le_bytes().as_ref()],
+        seeds=[b"wallet".as_ref(), user_sending.key().as_ref(), user_receiving.key().as_ref()],
         bump,
         token::mint = mint_of_token_being_sent,
-        token::authority = user_sending,
+        token::authority = escrow_pda,
     )]
     escrow_wallet: Account<'info, TokenAccount>,
 
@@ -104,4 +143,42 @@ pub struct State {
     pub token_mint: Pubkey,
     pub amount: u64,
     pub bump: u8,
+}
+
+#[derive(Accounts)]
+#[instruction(identifier: u64, pda_bump: u8)]
+pub struct PullBackInstruction<'info> {
+    #[account(
+        mut,
+        seeds=[b"state".as_ref(), user_sending.key().as_ref(), user_receiving.key.as_ref()],
+        bump,
+        //has_one = user_sending,
+        //has_one = user_receiving,
+    )]
+    escrow_pda: Account<'info, State>,
+    #[account(
+        mut,
+        seeds=[b"wallet".as_ref(), user_sending.key().as_ref(), user_receiving.key.as_ref()],
+        bump,
+    )]
+    escrow_wallet: Account<'info, TokenAccount>,    
+    // Users and accounts in the system
+    #[account(mut)]
+    user_sending: Signer<'info>,
+    /// CHECK: This is not a signer account
+    user_receiving: AccountInfo<'info>,
+    mint_of_token_being_sent: Account<'info, Mint>,
+
+    // Wallet to deposit to
+    #[account(
+        mut,
+        constraint=users_sending_token_account.owner == user_sending.key(),
+        constraint=users_sending_token_account.mint == mint_of_token_being_sent.key()
+    )]
+    users_sending_token_account: Account<'info, TokenAccount>,
+
+    // Application level accounts
+    system_program: Program<'info, System>,
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>,
 }
--- tests/treasury.ts
+++ tests/treasury.ts
@@ -2,22 +2,26 @@ import * as anchor from "@coral-xyz/anchor";
 import { BN, Program } from "@coral-xyz/anchor";
 import { Treasury } from "../target/types/treasury";
 import { Keypair, PublicKey } from "@solana/web3.js";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { expect } from "chai";
 
 anchor.setProvider(anchor.AnchorProvider.env());
 
-describe("treasury", () => {
-  const treasury = anchor.workspace.MyOracle as Program<Treasury>;
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+describe("treasury", async () => {
+  const treasury = anchor.workspace.Treasury as Program<Treasury>;
 
   const userSending = anchor.Wallet.local().payer;
   const userReceiving = Keypair.generate();
   const identifier = new BN(32213123);
 
-  const [escrowPda] = anchor.web3.PublicKey.findProgramAddressSync(
+  const [escrowPda, pdaBump] = anchor.web3.PublicKey.findProgramAddressSync(
     [
       anchor.utils.bytes.utf8.encode("state"),
       userSending.publicKey.toBuffer(),
       userReceiving.publicKey.toBuffer(),
-      identifier.toBuffer(),
+      //identifier.toBuffer(),
     ],
     treasury.programId
   );
@@ -27,24 +31,59 @@ describe("treasury", () => {
       anchor.utils.bytes.utf8.encode("wallet"),
       userSending.publicKey.toBuffer(),
       userReceiving.publicKey.toBuffer(),
-      identifier.toBuffer(),
+      //identifier.toBuffer(),
     ],
     treasury.programId
   );
 
+  const mint = new PublicKey("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU");
+  const bump = 4;
+
+  const userATA = await getAssociatedTokenAddress(mint, userSending.publicKey);
+
   it("Initialize escrow", async () => {
-    const tx = treasury.methods
-      .initialize(identifier, new BN(500000), 4) //0.5 USDC
+    const tx = await treasury.methods
+      .initialize(identifier, new BN(500000), pdaBump) //0.5 USDC
       .accounts({
         userSending: userSending.publicKey,
         userReceiving: userReceiving.publicKey,
-        mintOfTokenBeingSent: new PublicKey(
-          "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"
-        ),
+        mintOfTokenBeingSent: mint,
         escrowPda,
         escrowWallet,
-      });
+        usersSendingTokenAccount: userATA,
+      })
+      .rpc();
 
     console.log("Your transaction signature", tx);
+
+    await sleep(1000);
+
+    const escrowAccount = await treasury.account.state.fetch(escrowPda);
+
+    expect(escrowAccount.amount.toNumber()).to.be.equal(500000);
+    expect(escrowAccount.userSending.toBase58()).to.be.equal(
+      userSending.publicKey.toBase58()
+    );
+  });
+
+  it("PullBack funds", async () => {
+    const tx = await treasury.methods
+      .pullBack(identifier, pdaBump)
+      .accounts({
+        escrowPda,
+        escrowWallet,
+        userSending: userSending.publicKey,
+        userReceiving: userReceiving.publicKey,
+        mintOfTokenBeingSent: mint,
+        usersSendingTokenAccount: userATA,
+      })
+      .rpc();
+
+    console.log("Your transaction signature", tx);
+
+    await sleep(1000);
+
+    const escrowAccount = await treasury.account.state.fetch(escrowPda);
+    expect(escrowAccount.amount.toNumber()).to.be.equal(0);
   });
 });
